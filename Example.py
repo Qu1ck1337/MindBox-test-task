f4f6b38c7cf09f40bcb4116e007ce34e4d6b9bc9
Qu1ck1337 <77533886+Qu1ck1337@users.noreply.github.com>
2023-09-13 13:19:30 +0300

    Create Example.py

---
+++ Example.py
@@ -0,0 +1,9 @@
+from ShapesLib import Circle, Triangle
+
+#Initializing Circle class adding necessery radius argument, then using get_area()
+circle = Circle(100)
+print(circle.get_area())
+#31415.926535897932
+
+#To make simplier
+print(Triangle(10, 10, 10).get_area())
