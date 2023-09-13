dd14638b86aee1a11d64deb52f13f51983218a23
Qu1ck1337 <77533886+Qu1ck1337@users.noreply.github.com>
2023-09-13 13:32:36 +0300

    Update ShapesLib.py

--- ShapesLib.py
+++ ShapesLib.py
@@ -1,7 +1,7 @@
 import math
 
 
-#Абстрактный класс Shape, от которого будут унаследоваться Circle, Triangle
+# Abstract class Shape, from which Circle, Triangle will inherit
 class Shape:
     def __init__(self):
         self.area = 0
@@ -20,4 +20,14 @@ class Triangle(Shape):
     def __init__(self, side1: float, side2: float, side3: float):
         super().__init__()
         p = (side1 + side2 + side3) / 2
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
         self.area = math.sqrt(p * (p - side1) * (p - side2) * (p - side3))
+
+    def is_right_triangle(self) -> bool:
+        if (self.side1 ** 2 + self.side2 ** 2 == self.side3 ** 2) or \
+            (self.side2 ** 2 + self.side3 ** 2 == self.side1 ** 2) or \
+            (self.side1 ** 2 + self.side3 ** 2 == self.side2 ** 2):
+            return True
+        return False
